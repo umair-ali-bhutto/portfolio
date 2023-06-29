36fb5269c1b036f2de7e004ae28948fc27d90988
Your Name <umair2101f@aptechgdn.net>
2023-06-29 23:01:31 +0500

    Added Theme Files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# portfolio
+# Hello, I'm [Your Name]
+
+Welcome to my portfolio repository! This is where I showcase my work and provide information about my skills, experiences, and projects.
+
+## Table of Contents
+
+- [About Me](#about-me)
+- [Skills](#skills)
+- [Projects](#projects)
+- [Education](#education)
+- [Contact](#contact)
+
+## About Me
+
+Introduce yourself and provide a brief overview of who you are, your background, and your professional interests. Share what motivates you and what you're passionate about. You can also mention any personal hobbies or interests that are relevant to your work.
+
+## Skills
+
+List your core skills and competencies. Include both technical skills (programming languages, frameworks, tools) and soft skills (communication, problem-solving, teamwork) that are relevant to your professional profile.
+
+## Projects
+
+Highlight a selection of your notable projects. Briefly describe each project, including the problem it solves, your role in its development, and the technologies used. You can also include links to the repositories or live demos of the projects.
+
+## Education
+
+Provide information about your educational background. List the degrees or certifications you have obtained, the institutions you attended, and any relevant coursework or specialization.
+
+## Contact
+
+Share your contact information, such as your email address or social media handles, so that potential collaborators or employers can reach out to you. You can also include a link to your personal website or online portfolio for a more comprehensive view of your work.
---
+++ assets/js/app.js
@@ -0,0 +1,74 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
+  }
+
+  //Sctions Active
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //resmove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+
+  //Toggle theme
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
+}
+
+PageTransitions();
+
+function ConsolePrinting() {
+  console.log(
+    "%cHello, world!",
+    "color: blue; font-weight: bold; background-color: yellow;"
+  );
+
+  const data = [
+    { name: "John", age: 28, country: "USA" },
+    { name: "Emily", age: 32, country: "Canada" },
+    { name: "Anna", age: 24, country: "Germany" },
+  ];
+
+  console.table(data);
+
+  console.group("Group 1");
+  console.log("Message 1");
+  console.log("Message 2");
+  console.groupEnd();
+
+  console.group("Group 2");
+  console.log("Message 3");
+  console.log("Message 4");
+  console.groupEnd();
+}
+
+ConsolePrinting();
